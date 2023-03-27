96a756656acb5b21c4b1e587804723d68fb6b732
Yehor Romaniuk <krasheg@gmail.com>
2023-03-27 12:24:31 +0300

    created groups, posts apps; created templates for it

--- simplesocial/urls.py
+++ simplesocial/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('', views.HomePage.as_view(), name='home'),
     path('accounts/', include('accounts.urls', namespace='accounts')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('thanks/', views.ThanksPage.as_view(), name='thanks'),
 ]
--- simplesocial/views.py
+++ simplesocial/views.py
@@ -1,5 +1,9 @@
 from django.views.generic import TemplateView
 
 
+class ThanksPage(TemplateView):
+    template_name = 'thanks.html'
+
+
 class HomePage(TemplateView):
     template_name = 'index.html'
